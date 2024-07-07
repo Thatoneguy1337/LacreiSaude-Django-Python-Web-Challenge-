891c54cb678430129d95941b07530280a08e5635
victorguterres <vgb1337@gmail.com>
2024-07-07 17:04:46 -0300

    feat: professionals x schedules

--- professionals/urls.py
+++ professionals/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("professionals/", views.ProfessionalView.as_view()),
-    path("professionals/all/", views.ProfessionalAllView.as_view()),
-    path("professionals/<int:professional_id>/", views.ProfessionalViewDetail.as_view()),
+    path("professionals/", views.ProfessionalView.as_view(), name='professional-create' ),
+    path("professionals/all/", views.ProfessionalAllView.as_view(), name ='professional-list'),
+    path("professionals/<int:professional_id>/", views.ProfessionalViewDetail.as_view(), name='professional-detail' ),
 
 ] 
