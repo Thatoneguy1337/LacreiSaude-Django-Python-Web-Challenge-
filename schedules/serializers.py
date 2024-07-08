42d5c5693784acb94daec79c55ff25a983a65276
victorguterres <vgb1337@gmail.com>
2024-07-07 22:25:10 -0300

    fix: schedules x serializer

--- schedules/serializers.py
+++ schedules/serializers.py
@@ -2,15 +2,11 @@ from rest_framework import serializers
 from .models import Schedules
 
 class SchedulesSerializer(serializers.ModelSerializer):
+    professional_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Schedules
-        fields = [
-            "id",
-            "professional",
-            "appointment_date",
-            "created_at",
-            "updated_at",
-        ]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ['id', 'professional', 'professional_name', 'appointment_date', 'created_at', 'updated_at']
 
-    
+    def get_professional_name(self, obj):
+        return obj.professional.fullname
