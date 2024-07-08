5e8749d19539b964d2e538e3233e5ebd0add1289
victorguterres <vgb1337@gmail.com>
2024-07-07 21:10:37 -0300

    fix: filters user

--- schedules/urls.py
+++ schedules/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("schedules/", views.ScheduleCreateView.as_view(), name='schedule-create'),
     path("schedules/all/", views.ScheduleListView.as_view(), name='schedule-list'),
     path("schedules/<int:schedule_id>/", views.ScheduleDetailView.as_view(), name='schedule-detail'),
+    path("schedules/<int:schedule_id>/professional/", views.ScheduleDetailView.as_view(), name='schedule-professional'),
 ]
